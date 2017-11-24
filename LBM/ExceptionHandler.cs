784159978b6234bc0af7ffc37f61cf010b7d06ef
Edith Chow <edith.cy.chow@gmail.com>
2017-11-24 03:34:26 -0500

    New exception handler class - WIP, name TBD.

---
+++ LBM/ExceptionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LBM
+{
+    public class ExceptionHandler
+    {
+        public ExceptionHandler() { }
+
+        public static string GetExceptionDetails(Exception exc)
+        {
+            string errMsg = exc.Message;
+            errMsg += Environment.NewLine;
+            if (exc.InnerException != null)
+            {
+                errMsg += exc.InnerException.Message;
+                errMsg += Environment.NewLine;
+            }
+            errMsg += exc.StackTrace;    
+            return errMsg;
+        }
+    }
+}
